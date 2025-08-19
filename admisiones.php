6e6cff22d1e37ebe27817c071b189b67e851f320
Alexis <lopez.omar@iexe.edu.mx>
2025-08-19 12:12:26 -0600

    Admisiones -> Proceso inscripcion | Requisitos Admision | Proceso Convalidacion

--- admisiones.php
+++ admisiones.php
@@ -8,9 +8,18 @@
                     Comienza tu proceso de inscripción a IEXE Universidad
                 </h2>
                 <p class="proceso-inscripcion-descripcion">
-                    Estudia desde cualquier lugar del país con nuestra universidad 100 % en línea. Ofrecemos programas de calidad, avalados por la SEP, diseñados para adaptarse a tus tiempos y necesidades. Nuestro modelo educativo es flexible, accesible y con opciones de apoyo financiero y becas para que nada te impida alcanzar tu meta profesional.
+                    Estudia desde cualquier lugar del país con nuestra universidad 100 % en línea. Ofrecemos programas de calidad, avalados por la SEP, diseñados para adaptarse a tus tiempos y necesidades. 
+                    <br>
+                    Nuestro modelo educativo es flexible, accesible y con opciones de apoyo financiero y becas para que nada te impida alcanzar tu meta profesional.
                 </p>
-                <img class="mx-3" src="assets/img/admisiones/modelo-educativo.png" alt="">
+                 <p class="ms-5 proceso-inscripcion-descripcion proceso-inscripcion-detalles">
+                    <span class="requisitos-admision-item me-1"> </span>
+                    Este programa SEP en línea permite estudiar desde cualquier lugar, manteniendo un alto estándar académico y una formación aplicable desde el primer módulo.
+                 </p>
+                 <p class="ms-5 proceso-inscripcion-descripcion proceso-inscripcion-detalles">
+                    <span class="requisitos-admision-item me-1"> </span>
+                   Con más de una década de trayectoria, IEXE Universidad ha consolidado su experiencia mediante la realización de más de 350 evaluaciones de políticas públicas en América Latina. 
+                 </p>
                 <div class="d-flex">
                     <div class="d-flex flex-column justify-content-center align-items-center mx-5">
                         <img src="assets/img/becas/calculadora.png" alt="">
@@ -58,17 +67,17 @@
 
 <section id="section-requisitos-admision">
     <div class="container section-requisitos-admision">
-        <div class="row">
-            <div class="col-3">
+        <div class="row admision-programas-contenedor d-flex flex-column justify-content-end">
+            <div class="col-3 d-flex justify-content-center align-items-center admision-programa-academico admision-programa-academico-seleccionado">
                 Licenciaturas (Estatal)
             </div>
-            <div class="col-3">
+            <div class="col-3 d-flex justify-content-center align-items-center admision-programa-academico">
                 Licenciaturas (Federal)
             </div>
-            <div class="col-3">
+            <div class="col-3 d-flex justify-content-center align-items-center admision-programa-academico">
                 Maestrías (Estatal)
             </div>
-            <div class="col-3">
+            <div class="col-3 d-flex justify-content-center align-items-center admision-programa-academico">
                 Maestrías (Federal)
             </div>
         </div>
@@ -81,10 +90,60 @@
                     <p> <span class="requisitos-admision-item me-1"></span>Fotografía digital.</p>
                 </div>
             </div>
-            <div class="col-6 d-flex justify-content-center">
-                <div>
-                    <img src="assets/img/admisiones/requisitos.png" alt="">
+            <div class="col-6 d-flex justify-content-center contenedor-requisitos-admision-fondo">
+                <div class="w-100 d-flex justify-content-center me-5 contenedor-requisitos-admision-imagen">
+                    <img class="img-fluid" src="assets/img/admisiones/requisitos.png" alt="">
                 </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section id="section-convalidacion-internacional">
+    <div class="container section-convalidacion-internacional pt-3">
+        <div class="row px-5">
+            <div class="col-12">
+                <h2 class="convalidacion-internacional-titulo">Proceso de convalidación internacional</h2>
+                <h3 class="convalidacion-internacional-subtitulo">En los siguientes enlaces podrás encontrar la información necesaria de cada país para realizar tu proceso de convalidación.</h3>
+            </div>
+        </div>
+        <div class="row mt-4">
+            <div class="col-2 d-flex flex-column justify-content-center align-items-center">
+                <h4 class="convalidacion-internacionaL-dependencia">SENECYT</h4>
+                <h5 class="convalidacion-internacionaL-pais">Ecuador</h5>
+                <img src="assets/img/admisiones/bandera-ecuador.png" alt="">
+                <img src="assets/img/admisiones/ver_proceso.png" alt="">
+            </div>
+            <div class="col-2 d-flex flex-column justify-content-center align-items-center">
+                <h4 class="convalidacion-internacionaL-dependencia">MINEDUCAC</h4>
+                <h5 class="convalidacion-internacionaL-pais">Colombia</h5>
+                <img src="assets/img/admisiones/bandera-colombia.png" alt="">
+                <img src="assets/img/admisiones/ver_proceso.png" alt="">
+            </div>
+            <div class="col-2 d-flex flex-column justify-content-center align-items-center">
+                <h4 class="convalidacion-internacionaL-dependencia">USAC</h4>
+                <h5 class="convalidacion-internacionaL-pais">Guatemala</h5>
+                <img src="assets/img/admisiones/bandera-guatemala.png" alt="">
+                <img src="assets/img/admisiones/ver_proceso.png" alt="">
+            </div>
+            <div class="col-2 d-flex flex-column justify-content-center align-items-center">
+                <h4 class="convalidacion-internacionaL-dependencia">MINED</h4>
+                <h5 class="convalidacion-internacionaL-pais">El Salvador</h5>
+                <img src="assets/img/admisiones/bandera-el-salvador.png" alt="">
+                <img src="assets/img/admisiones/ver_proceso.png" alt="">
+            </div>
+            <div class="col-2 d-flex flex-column justify-content-center align-items-center">
+                <h4 class="convalidacion-internacionaL-dependencia">MEDUCA</h4>
+                <h5 class="convalidacion-internacionaL-pais">Panamá</h5>
+                <img src="assets/img/admisiones/bandera-panama.png" alt="">
+                <img src="assets/img/admisiones/ver_proceso.png" alt="">
+            </div>
+            <div class="col-2 d-flex flex-column justify-content-center align-items-center">
+                <h4 class="convalidacion-internacionaL-dependencia">MINEDU</h4>
+                <h5 class="convalidacion-internacionaL-pais">Perú</h5>
+                <img src="assets/img/admisiones/bandera-peru.png" alt="">
+                <img src="assets/img/admisiones/ver_proceso.png" alt="">
             </div>
         </div>
     </div>
